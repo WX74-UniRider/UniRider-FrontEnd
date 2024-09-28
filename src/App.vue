65bc2a08e1e262c2707ce93e268bb1abae1879eb
Valentino Sebastian Cervantes Erequita <u202110140@upc.edu.pe>
2024-09-27 20:59:18 -0500

    feat: Adding components

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,33 @@ import ChooseUser from "./unirider/shared/pages/choose-user.vue";
 import ChatUser from "./unirider/shared/pages/chat-user.vue";
 import Confirmation from "./unirider/shared/pages/confirmation.component.vue";
 import ProfileUser from "./unirider/shared/pages/Profile-user.component.vue";
+import appConductorFinderCardComponent from "./unirider/shared/components/app-conductor-finder-card/app-conductor-finder-card.component.vue";
+import AppConductorFinderComponent from "./unirider/shared/pages/conductor-finder.component.vue"
+import AppBookingFastForm from "./unirider/shared/components/app-booking-fast-form/app-booking-fast-form.component.vue";
+
 
 export default {
   components: {
+    AppBookingFastForm,
     ProfileUser,
     Confirmation,
     ChatUser,
     ChooseUser,
-    LoginUser
+    LoginUser,
+    appConductorFinderCardComponent,
+    AppConductorFinderComponent
   }
 }
 </script>
 
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <!--AppConductorFinderComponent></--AppConductorFinderComponent>-->
+
+  <AppBookingFastForm
+      :passengerId=1
+      :driverId=1
+      :destination="SanMiguel"
+  />
 
   <!--ChatUser/>-->
 
