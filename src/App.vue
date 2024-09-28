08cb1df128b6142937e89b8ebee9af8448a347d1
anonimat0 <u202114714@upc.edu.pe>
2024-09-27 21:52:02 -0500

    feat: update app.vue

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,22 @@ import ChooseUser from "./unirider/shared/pages/choose-user.vue";
 import ChatUser from "./unirider/shared/pages/chat-user.vue";
 import Confirmation from "./unirider/shared/pages/confirmation.component.vue";
 import ProfileUser from "./unirider/shared/pages/Profile-user.component.vue";
-import appConductorFinderCardComponent from "./unirider/shared/components/app-conductor-finder-card/app-conductor-finder-card.component.vue";
-import AppConductorFinderComponent from "./unirider/shared/pages/conductor-finder.component.vue"
-import AppBookingFastForm from "./unirider/shared/components/app-booking-fast-form/app-booking-fast-form.component.vue";
-
 
 export default {
   components: {
-    AppBookingFastForm,
     ProfileUser,
     Confirmation,
     ChatUser,
     ChooseUser,
-    LoginUser,
-    appConductorFinderCardComponent,
-    AppConductorFinderComponent
+    LoginUser
   }
 }
 </script>
 
 <template>
-  <!--AppConductorFinderComponent></--AppConductorFinderComponent>-->
-
-  <AppBookingFastForm
-      :passengerId=1
-      :driverId=1
-      :destination="SanMiguel"
-  />
+  <div id="app">
+    <router-view/>
+  </div>
 
   <!--ChatUser/>-->
 
