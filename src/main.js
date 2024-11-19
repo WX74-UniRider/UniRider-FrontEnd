4661e7ab11a91eba21db2602b5b788c2d1723fde
anonimat0 <u202114714@upc.edu.pe>
2024-11-19 16:04:41 -0500

    feat: add tailwind in the project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './style.css'
 import { i18n } from './i18n.js';
 import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/aura-light-green/theme.css';
