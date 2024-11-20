2292f4aa782328398cffbd4211a10f6d5b90fc25
anonimat0 <u202114714@upc.edu.pe>
2024-11-19 20:30:54 -0500

    feat: update dependencys

--- src/main.js
+++ src/main.js
@@ -1,34 +1,51 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './style.css'
+import { createApp } from 'vue';
+import App from './app.vue';
+import router from "./router/index.js";
+import './style.css';
 import { i18n } from './i18n.js';
-import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/aura-light-green/theme.css';
 
+// PrimeVue
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'primeicons/primeicons.css'; // PrimeIcons para íconos
+import 'primeflex/primeflex.css'; // PrimeFlex para clases utilitarias
+
+// PrimeVue Components
 import InputText from 'primevue/inputtext';
 import Panel from 'primevue/panel';
 import Button from 'primevue/button';
 import Card from 'primevue/card';
+import Select from 'primevue/select';
 import Password from 'primevue/password';
-
-import { GoogleMap } from 'vue3-google-map'
 import Paginator from 'primevue/paginator';
 
-createApp(App)
-    .use(router)
-    .use(PrimeVue)
-    .use(i18n)
-    .use(GoogleMap, {
-        load: {
-            key: 'AIzaSyBWlzfRtgnZqbDJfUg0mvaSaumzuwhVHYI',
-            libraries: ['places'],
-        },
-    })
-    .component('pv-input-text', InputText)
+// Google Maps
+import { GoogleMap } from 'vue3-google-map';
+
+// Crear instancia de la aplicación
+const app = createApp(App);
+
+// Configuración y uso de PrimeVue
+app.use(PrimeVue, { theme: { preset: Aura }, ripple: true });
+
+// Uso de plugins adicionales
+app.use(router);
+app.use(i18n);
+app.use(GoogleMap, {
+    load: {
+        key: 'AIzaSyBWlzfRtgnZqbDJfUg0mvaSaumzuwhVHYI',
+        libraries: ['places'],
+    },
+});
+
+// Registro de componentes de PrimeVue
+app.component('pv-input-text', InputText)
     .component('pv-panel', Panel)
     .component('pv-button', Button)
     .component('pv-card', Card)
     .component('pv-password', Password)
-    .component('pv-paginator', Paginator)
-    .mount('#app')
+    .component('pv-select', Select)
+    .component('pv-paginator', Paginator);
+
+// Montar la aplicación
+app.mount('#app');
